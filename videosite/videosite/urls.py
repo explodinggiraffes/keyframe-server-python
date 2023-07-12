756742f666455c88a4538109683fbfb8616d85df
John Koszarek <john.koszarek@gmail.com>
2023-07-12 17:44:16 -0500

    Whitespace.

--- videosite/videosite/urls.py
+++ videosite/videosite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path("videos/", include("videos.urls")),
     path("admin/", admin.site.urls),
